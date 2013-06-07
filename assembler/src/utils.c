df61b892aefcb19c1dd36a01b1f3b83c40250125
Pavan Pinnaka <pp2112@visual11.doc.ic.ac.uk>
2013-06-07 15:28:04 +0100

    Finished File Writer

--- assembler/src/utils.c
+++ assembler/src/utils.c
@@ -12,14 +12,14 @@
 #include "LinkedList.h"
 
 
-void writeToBinaryFile(char *filePath,assembler assInstState){
+void writeToBinaryFile(char *filePath,assembler *assInstState){
  FILE *fp = fopen(filePath,"w");
  if(fp == NULL){
   perror("Writing binary file error");
   exit(EXIT_FAILURE);
  }
- for(int i=0; assInstState->Instructions[i] != 0x01; i++){
-  fwrite(fp,assInstState->Instructions[i]);
+ for(int i=0; (assInstState->Instructions[i]) != 0x01; i++){
+  fwrite(&(assInstState->Instructions[i]),sizeof(uint32_t),1,fp);
  }
 }
 
